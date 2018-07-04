480935672732e48094d7c0efc42e5caf720f7ac7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-07-04 09:17:01 +0200

    Initial implementation of generator logic

--- src/test/java/nl/dvberkel/balanx/TicTacToeTest.java
+++ src/test/java/nl/dvberkel/balanx/TicTacToeTest.java
@@ -4,14 +4,19 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static nl.dvberkel.balanx.TicTacToe.Position.*;
+import static nl.dvberkel.balanx.TicTacToeBuilder.board;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsNot.not;
 
 public class TicTacToeTest {
     @Test
-    public void emptyBoardShouldHaveSomeMoves() {
+    public void emptyBoardShouldHaveAllPositionsPlayable() throws DuplicateTicTacToePositionPlacementException {
         MoveGenerator<TicTacToe> generate = TicTacToeGenerator.instance();
         TicTacToe emptyBoard = TicTacToe.empty();
 
@@ -19,13 +24,147 @@ public class TicTacToeTest {
 
         assertThat(nextMoves, is(not(nullValue())));
         assertThat(nextMoves, is(not(empty())));
+        assertThat(nextMoves, contains(
+                board().crossAt(NW).build(),
+                board().crossAt(N).build(),
+                board().crossAt(NE).build(),
+                board().crossAt(W).build(),
+                board().crossAt(C).build(),
+                board().crossAt(E).build(),
+                board().crossAt(SW).build(),
+                board().crossAt(S).build(),
+                board().crossAt(SE).build()
+        ));
+    }
+}
+
+class TicTacToeBuilder {
+    public static TicTacToeBuilder board() {
+        return new TicTacToeBuilder();
+    }
+
+    private TicTacToe board;
+
+    private TicTacToeBuilder() {
+        this.board = TicTacToe.empty();
+    }
+
+    public TicTacToeBuilder crossAt(TicTacToe.Position position) throws DuplicateTicTacToePositionPlacementException {
+        Optional<TicTacToe> candidate = position.place(this.board);
+        if (candidate.isPresent()) {
+            this.board = candidate.get();
+            return this;
+        } else {
+            throw new DuplicateTicTacToePositionPlacementException(position);
+        }
+    }
+
+    public TicTacToe build() {
+        return this.board;
+    }
+}
+
+class DuplicateTicTacToePositionPlacementException extends Exception {
+    public DuplicateTicTacToePositionPlacementException(TicTacToe.Position position) {
+        super(String.format("token is already placed at position %s", position));
+    }
+}
+
+class IllegalPositionIndexException extends RuntimeException {
+    public IllegalPositionIndexException(int index) {
+        super(String.format("index %d is not a valid Position index", index));
     }
 }
 
 class TicTacToe {
+    public static enum Position {
+        NW(0), N(1), NE(2),
+        W(3),  C(4), E(5),
+        SW(6), S(7), SE(8);
+
+        private int index;
+
+        Position(int index) {
+            this.index = index;
+        }
+
+        public Optional<TicTacToe> place(TicTacToe board) {
+            return board.place(index);
+        }
+
+        public static Position from(int index) {
+            for (Position candidate : Position.values()) {
+                if (candidate.index == index) {
+                    return candidate;
+                }
+            }
+            throw new IllegalPositionIndexException(index);
+        }
+    }
+
+    static enum Token {
+        Cross,
+        Dot,
+        Empty
+    }
 
     public static TicTacToe empty() {
-        return null;
+        return new TicTacToe();
+    }
+
+    private final Token[] tokens;
+
+    private TicTacToe() {
+        this(new Token[]{
+                Token.Empty, Token.Empty, Token.Empty,
+                Token.Empty, Token.Empty, Token.Empty,
+                Token.Empty, Token.Empty, Token.Empty,
+        });
+    }
+
+    private TicTacToe(Token[] tokens) {
+        this.tokens = tokens;
+    }
+
+    private Optional<TicTacToe> place(int index) {
+        if (tokens[index].equals(Token.Empty)) {
+            Token[] placedTokens = Arrays.copyOf(tokens, tokens.length);
+            placedTokens[index] = Token.Cross;
+            return Optional.of(new TicTacToe(placedTokens));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public List<Position> playablePositions() {
+        return IntStream.range(0, tokens.length)
+                .filter(index -> tokens[index].equals(Token.Empty))
+                .mapToObj(index -> Position.from(index))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TicTacToe ticTacToe = (TicTacToe) o;
+
+        return IntStream.range(0, tokens.length)
+                .filter(index -> !tokens[index].equals(ticTacToe.tokens[index]))
+                .count() == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(tokens);
+    }
+
+    @Override
+    public String toString() {
+        return "TicTacToe{" +
+                "tokens=" + Arrays.toString(tokens) +
+                '}';
     }
 }
 
@@ -37,7 +176,11 @@ class TicTacToeGenerator implements MoveGenerator<TicTacToe> {
 
     @Override
     public List<TicTacToe> movesFor(TicTacToe state) {
-        return Arrays.asList(state);
+        List<TicTacToe.Position> playable = state.playablePositions();
+        return playable
+                .stream()
+                .map(position -> position.place(state).get())
+                .collect(Collectors.toList());
     }
 }
 
